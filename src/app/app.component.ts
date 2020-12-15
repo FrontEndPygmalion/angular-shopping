d61a0398c56d0259f89259d3b5db17e923d0c43f
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-12-14 21:39:48 -0500

    Uso de directivas NG for, switch, style, class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,5 +19,22 @@ export class AppComponent {
     this.usuarios.pop()
   }
 
+  productos = [
+    {
+      id: 1,
+      image: 'https://www.mercadoslpineda.co/1749-large_default/quesito-colanta-x-400-g-montefrio.jpg',
+      name: 'quesito',
+      description: 'delicioso quesito',
+      price: 2500
+    },
+    {
+      id: 2,
+      image: 'https://www.vivosano.org/wp-content/uploads/beneficios-chocolate.jpg',
+      name: 'chocolate',
+      description: 'delicioso quesito',
+      price: 2500
+    }
+  ]
+
 
 }
